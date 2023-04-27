82dea2e71f863b1419ab52fd4c9d75ee31895f2d
Tweek36 <74772088+Tweek36@users.noreply.github.com>
2023-04-27 17:29:48 +0300

    1

--- web_project/settings.py
+++ web_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c%t4di1$1u!o_afies3r8j8lr%hj=r73mu&4a!j08j9vid+cy^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
