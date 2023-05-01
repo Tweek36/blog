04738f2d4f4ae985316e880bbd6d513fcdc53f7f
Tweek36 <74772088+Tweek36@users.noreply.github.com>
2023-05-01 14:37:00 +0300

    wsgi update

--- web_project/wsgi.py
+++ web_project/wsgi.py
@@ -8,10 +8,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/Tweek36/blog'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+
+#from django.core.wsgi import get_wsgi_application
+#
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
+
+#application = get_wsgi_application()
+#app = application
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
-
 application = get_wsgi_application()
-app = application
