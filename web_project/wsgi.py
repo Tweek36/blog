fb451c72e599ed6b06044aadde00385baacb78f8
Tweek36 <74772088+Tweek36@users.noreply.github.com>
2023-05-01 14:41:11 +0300

    wsgi fix

--- web_project/wsgi.py
+++ web_project/wsgi.py
@@ -14,7 +14,7 @@ path = '/home/Tweek36/blog'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'blog.settings'
 
 
 #from django.core.wsgi import get_wsgi_application
